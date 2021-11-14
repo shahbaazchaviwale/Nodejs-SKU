66fe67f936d194317463129c582aa8392612b9d7
Shahbaaz <shahbaazc@gmail.com>
2021-11-14 11:29:10 +0530

    created magic string to avoid bug without magic string  , difiiculty to find bug using tools as string not rely on variable

---
+++ code-practice/Event-emitterS/02-0-config.js
@@ -0,0 +1,7 @@
+// defining magic string to avoid bugs
+//created magic string to avoid bug without magic string  , difiiculty to find bug using tools as string not rely on variable
+module.exports = {
+    events:{
+        GREET: 'greet'
+    }
+}
