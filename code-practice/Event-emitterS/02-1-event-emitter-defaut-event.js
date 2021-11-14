3e279e98f7588f2052cbc6c10d523531f35c0baa
Shahbaaz <shahbaazc@gmail.com>
2021-11-14 11:26:29 +0530

    added default event of nodejs instead of creating emitter constructor

---
+++ code-practice/Event-emitterS/02-1-event-emitter-defaut-event.js
@@ -0,0 +1,26 @@
+// added default event of nodejs instead of creating emitter constructor
+const Emitter = require('events');
+const configEvent = require('./02-0-config').events;
+
+var emtr = new Emitter();
+// call maultiple times here
+emtr.on(configEvent.GREET, function(){
+    console.log('Greet-1 called !!');
+});
+
+emtr.on('greet', function(){
+    console.log('Greet-2 called !!');
+});
+
+emtr.on('greet', function(){
+    console.log('Greet-3 called !!');
+});
+
+
+emtr.on('greet-2', function(){
+    console.log('Greet-4 called !!');
+});
+console.log('------------------------');
+// name must be same as on "on" events 
+emtr.emit('greet');
+emtr.emit('greet-2');
